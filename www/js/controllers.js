882055efa905da2ee2a636cc9163fc9fa37b1ded
root <root@localhost.localdomain>
2015-09-08 01:22:04 -0500

    Implementacón compra de varios platos por mesa

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -48,7 +48,7 @@ app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 // CONTROLADORES DE LA APP
 
 // controlador para la vista home, encargada de recoger el código del restaurante
-app.controller('HomeCtrl', function($scope, localStorageService, $location) {
+app.controller('HomeCtrl', function($scope, localStorageService, $location, $ionicHistory, $state) {
   //$scope.pin = 0;
   // almaceno el código del restaurante en el local storage
   $scope.saveCodigoRestaurante = function(){
@@ -58,7 +58,11 @@ app.controller('HomeCtrl', function($scope, localStorageService, $location) {
 
     if (codigo != 0) {
       localStorageService.set('codigoRestaurante', codigo);
-      $location.url('/app/mesa');
+
+      $ionicHistory.clearHistory();
+      $ionicHistory.clearCache();
+      $state.go('app.mesa');
+      //$location.url('/app/mesa');
     }else{
       $scope.respuesta_codigo = 'Ingrese el código del restaurante';
     }
@@ -68,7 +72,7 @@ app.controller('HomeCtrl', function($scope, localStorageService, $location) {
 
 
 // controlador para capturar el numero de la mesa y alamcenarlo en el local storage para su posterior uso
-app.controller('MesaCtrl', function($scope, localStorageService, $location){
+app.controller('MesaCtrl', function($scope, localStorageService, $location,$ionicHistory, $state){
 
   //$scope.mesa = 0;
   // función para almacenar el numero de la mesa en el local storage
@@ -80,7 +84,12 @@ app.controller('MesaCtrl', function($scope, localStorageService, $location){
       localStorageService.set('numMesa', numMesa);
       localStorageService.set('count', 1); // contador
       //nos vamos al estado menuPrincipal 
-      $location.url('/app/menuPrincipal');
+      $ionicHistory.clearHistory();
+      $ionicHistory.clearCache();
+      $state.go('app.menuPrincipal');
+
+      //$location.url('/app/menuPrincipal');
+
     }else{
       $scope.respuesta_mesa = 'Ingrese el numero de su mesa';
     }
@@ -109,11 +118,14 @@ app.controller('CategoriasCtrl', function($scope, localStorageService, Menu_cate
 
 
 // controlador para gestionar el menu principal de la app
-app.controller('MenuPrincipalCtrl', function($scope, $location, Menu_categorias){
+app.controller('MenuPrincipalCtrl', function($scope, $location, Menu_categorias, $ionicHistory, $state){
   
   // vamos a menu-categorias.html
   $scope.verMenu = function(){
-    $location.url('/app/menuCategorias');
+    $ionicHistory.clearHistory();
+    $ionicHistory.clearCache();
+    $state.go('app.menuCategorias');
+    //$location.url('/app/menuCategorias');
     console.log(Menu_categorias);
   }
 
@@ -141,7 +153,7 @@ app.controller('MenuPrincipalCtrl', function($scope, $location, Menu_categorias)
 
 
 // controlador para gestionar el menú categorías, desde acá se cargan todas las categorías del restaurante
-app.controller('MenuCategoriasCtrl', function($scope, $location, Menu_categorias, localStorageService){
+app.controller('MenuCategoriasCtrl', function($scope, $location, Menu_categorias, localStorageService, $ionicHistory, $state){
   $scope.categorias = Menu_categorias;
 
   // función para cargar la vista de platos pertenecientes a una categoría
@@ -149,14 +161,18 @@ app.controller('MenuCategoriasCtrl', function($scope, $location, Menu_categorias
     console.log('Id Categoria: ' + idCategoria);
 
     localStorageService.set('idCategoria', idCategoria);
-    $location.url('/app/platos');
+
+    $ionicHistory.clearHistory();
+    $ionicHistory.clearCache();
+    $state.go('app.platos');
+    //$location.url('/app/platos');
   };
 
 });
 
 
 // controlador para gestionar los platos de una categoría
-app.controller('PlatosCtrl', function($scope, $location, localStorageService){
+app.controller('PlatosCtrl', function($scope, $location, localStorageService, $ionicHistory, $state){
   var id = localStorageService.get('idCategoria'); // accedo al id de la categoría seleccionada por el usuario
   $scope.platos = null; //limpio el $scope de platos
 
@@ -169,9 +185,9 @@ app.controller('PlatosCtrl', function($scope, $location, localStorageService){
     listPlatos[count] = plato.val();
     listPlatos[count].$id = plato.key();
     $scope.platos = listPlatos.filter(Boolean);
-    console.log('Listado de platos');
-    console.log($scope.platos);
   });
+  console.log('Listado de platos');
+    console.log($scope.platos);
 
 
   //funcion para mostrara la visata y la info del plato seleccionado por el usuario
@@ -179,29 +195,21 @@ app.controller('PlatosCtrl', function($scope, $location, localStorageService){
     console.log('ID Plato Seleccionado: '+ idPlato);
     localStorageService.set('idPlato', idPlato);
 
-    $location.url('/app/platoSeleccionado');
+    $ionicHistory.clearHistory();
+    $ionicHistory.clearCache();
+    $state.go('app.platoSeleccionado');
+    //$location.url('/app/platoSeleccionado');
   };
 });
 
 
 // controlador para gestionar los platos seleccioniados en una mesa
-app.controller('platoSeleccionadoCtrl', function($scope, $location, localStorageService, Pedidos){
-  // octener los pedidos de la mesa que estan en el local storage
-  var contador = localStorageService.get('count');
-
-  var pedidosMesa = [];
-
-  for (var i = 0; i <= contador; i++) {
-    pedidosMesa[i] = localStorageService.get('pedido'+i);
-    console.log(pedidosMesa);
-  };
-
+app.controller('platoSeleccionadoCtrl', function($scope, $location, localStorageService, Pedidos, $ionicHistory, $state){
 
   var id = localStorageService.get('idPlato');
 
   var plato = new Firebase("https://tucocina.firebaseio.com/platos/"+id).once('value', function(data){
     $scope.platoSelect = data.val();
-    console.log('Plato: '+$scope.platoSelect);
   });
 
   //ingredientes de un plato
@@ -214,8 +222,6 @@ app.controller('platoSeleccionadoCtrl', function($scope, $location, localStorage
     ingredientes[count] = ingrediente.val();
     ingredientes[count].$id = ingrediente.key();
     $scope.ingredientesPlato = ingredientes.filter(Boolean);
-    console.log('Listado de ingredientes');
-    console.log($scope.ingredientesPlato);
   });
 
   // adicionales de un plato
@@ -228,8 +234,6 @@ app.controller('platoSeleccionadoCtrl', function($scope, $location, localStorage
     adicionales[count] = adicional.val();
     adicionales[count].$id = adicional.key();
     $scope.adicionalesPlato = adicionales.filter(Boolean);
-    console.log('Listado de adicionales');
-    console.log($scope.adicionalesPlato);
   });
 
 
@@ -242,11 +246,9 @@ app.controller('platoSeleccionadoCtrl', function($scope, $location, localStorage
   miMesa.orderByChild("mesa").equalTo(numMesa).on("child_added", function(mesa) {
     count++;
     mesaActual[count] = mesa.val();
-    console.log(mesaActual);
+    //console.log(mesaActual);
     // mesaActual[count].$id = mesa.key();
     $scope.pedidosMesa = mesaActual.filter(Boolean);
-    console.log('Listado de platos de la mesa');
-    console.log($scope.pedidosMesa);
   });
 
 
@@ -264,37 +266,81 @@ app.controller('platoSeleccionadoCtrl', function($scope, $location, localStorage
     };
 
     localStorageService.set('pedido'+count, pedido);
+
     count++;
     localStorageService.set('count', count);
 
-     $location.url('app/resumen');
+    $ionicHistory.clearHistory();
+    $ionicHistory.clearCache();
+    $state.go('app.resumen');
+     //$location.path('app/resumen');
     }
-  
 
-  // función para realizar un nuevo pedido en la mesa actual
-  $scope.otroPedido = function(){
-    $location.url('/app/menuCategorias');
-  };
+});
 
+// controlador para mostrar el resumen de pedidos de la mesaActual
+app.controller('ResumenCtrl', function($scope, $location, localStorageService, Pedidos, $ionicLoading, $timeout, $ionicHistory, $state){
+   // Setup the loader
+    $ionicLoading.show({
+      content: 'Loading',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+    });
 
-  // función para enviar el pedido, despúes de esto el pedido llegará al mesero
+    $timeout(function () {
+      $ionicLoading.hide();
+    // obtener los pedidos de la mesa que estan en el local storage
+      var count = localStorageService.get('count');
+      var pedidosMesaActual = [];
+
+      for (var i = 0; i <= count; i++) {
+        pedidosMesaActual[i] = localStorageService.get('pedido'+i);
+      };
+
+      $scope.misPedidos = pedidosMesaActual.filter(Boolean);
+      console.log('PEDIDOS DE LA MESA');
+      console.log($scope.misPedidos);
+  }, 1000);
+
+   // función para realizar un nuevo pedido en la mesa actual
+    $scope.otroPedido = function(){
+      //$location.url('/app/menuCategorias');
+      $ionicHistory.clearHistory();
+      $ionicHistory.clearCache();
+
+      $state.go('app.menuCategorias');
+    };
+
+  // función para enviar el pedido, despúes de esto elfilter pedido llegará al mesero
   // luego de confirmar el envío se le redireccionará al resumen desde donde podrá realizar 
   // un nuevo pedido que quedará asignado a la mesa actual
   $scope.enviarPedido = function(){
     var mesa = localStorageService.get('numMesa');
+     // obtener los pedidos de la mesa que estan en el local storage
+    var count = localStorageService.get('count');
+    var pedidoFinal = [];
 
-    var pedido = {
-      mesa: mesa,
-      plato: $scope.platoSelect.nombrePlato,
-      precio: $scope.platoSelect.valor,
-      estado: 'en proceso',
-      ingredientes: [$scope.ingredientesPlato],
-      adicionales: [$scope.adicionalesPlato]
+    for (var i = 0; i <= count; i++) {
+      pedidoFinal[i] = localStorageService.get('pedido'+i);
     };
 
-    Pedidos.$add(pedido);
+    $scope.miPedidoFianal = pedidoFinal.filter(Boolean);
+    console.log($scope.miPedidoFianal);
+
+    $scope.miPedidoFianal.mesa = mesa;
+
+    Pedidos.$add($scope.miPedidoFianal);
+
+    // ciclo para eliminar los datos del local storage
+    for (var i = 0; i <= count; i++) {
+      localStorageService.remove('idCategoria', 'idPlato', 'pedido'+i);
+    };
+
+    localStorageService.set('count', 1)
+
 
   };
-
 
 });
